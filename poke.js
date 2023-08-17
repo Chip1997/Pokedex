c71323153f6f462b8a920aaeff611bf4d92c2ec1
Javier Idiarte <99035124+Chip1997@users.noreply.github.com>
2023-08-17 16:59:25 -0300

    update

--- poke.js
+++ poke.js
@@ -1,51 +1,63 @@
 const pokemonListElement = document.getElementById('pokemon-list');
 const pokemonDetailsElement = document.getElementById('pokemon-details');
-const url = 'https://pokeapi.co/';
-// Función para obtener la lista de Pokémon desde la API
-function getPokemonList() {
-  fetch('https://pokeapi.co/api/v2/pokemon?limit=20')
-    .then(response => response.json())
-    .then(data => {
-      const pokemonList = data.results;
-      pokemonList.forEach(pokemon => {
-        const pokemonName = pokemon.name;
-        const pokemonUrl = pokemon.url;
-        const pokemonElement = document.createElement('div');
-        pokemonElement.textContent = pokemonName;
-        pokemonElement.addEventListener('click', () => {
-          getPokemonDetails(pokemonUrl);
-        });
-        pokemonListElement.appendChild(pokemonElement);
-      });
-    })
-    .catch(error => {
-      console.log('Error:', error);
-    });
-}
+const searchInput = document.getElementById('search-input');
+const searchButton = document.getElementById('search-button');
 
-// Función para obtener los detalles de un Pokémon específico desde la API
-function getPokemonDetails(url) {
-  fetch(url)
+function ObtenerPokemonPorNombre(name) {
+  fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
     .then(response => response.json())
     .then(data => {
       const pokemonName = data.name;
       const pokemonImage = data.sprites.front_default;
+      const pokemonBackImage = data.sprites.back_default;
       const pokemonTypes = data.types.map(type => type.type.name).join(', ');
-      const pokemonAbilities = data.abilities.map(ability => ability.ability.name).join(', ');
-
+      const pokemonAbilities = data.abilities.map(ability => ability.ability.name).join(',<br> ');
       pokemonDetailsElement.innerHTML = `
-        <h2>${pokemonName}</h2>
-        <img src="${pokemonImage}" alt="${pokemonName}">
-        <p><strong>Type(s):</strong> ${pokemonTypes}</p>
-        <p><strong>Abilities:</strong> ${pokemonAbilities}</p>
+        <div class="pokemon-info">
+          <h2>${pokemonName}</h2>
+          <img src="${pokemonImage}" alt="${pokemonName}">
+          <img src="${pokemonBackImage}" alt="${pokemonName}">
+        </div>
+        <div class="pokemon-data">
+          <p><strong>Tipo:</strong> <br> ${pokemonTypes}</p>
+          <p><strong>Habilidades:</strong> <br> ${pokemonAbilities}</p>
+        </div>
       `;
     })
     .catch(error => {
+      pokemonDetailsElement.innerHTML = '<p>Intenta con otro.</p>';
       console.log('Error:', error);
     });
 }
 
-// Obtener la lista de Pokémon al cargar la página
+function handleSearch() {
+  const pokemonName = searchInput.value.trim().toLowerCase();
+  if (pokemonName !== '') {
+    ObtenerPokemonPorNombre(pokemonName);
+  }
+}
+
+searchButton.addEventListener('click', handleSearch);
+
+searchInput.addEventListener('keyup', event => {
+  if (event.keyCode === 13) {
+    handleSearch();
+  }
+});
+
 document.addEventListener('DOMContentLoaded', () => {
-  getPokemonList();
+ 
+});
+
+var playButton = document.getElementById('playButton');
+var audio = document.getElementById('intro');
+
+playButton.addEventListener('click', function() {
+  if (audio.paused) {
+    audio.play();
+    playButton.innerHTML = 'Off';
+  } else {
+    audio.pause();
+    playButton.innerHTML = 'ON';
+  }
 });
